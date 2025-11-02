44e25ae5a9722a6a5a0ab9ffb79a07d15c90dc06
синечка ♡ <63897347+ktnk-dev@users.noreply.github.com>
2025-11-02 16:34:07 +0300

    срочный фикс

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main(page: ft.Page):
     page.title = 'Расписание'
     page.spacing = 0
     settings.settheme(page, None)
-    setup.login(page, True)
+    setup.login(page)
     setup.navigator(page)
     
 ft.app(main)
