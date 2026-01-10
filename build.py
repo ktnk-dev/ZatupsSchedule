f1facf6b7b1cb2ecad0f5499655072dc1512328b
синечка ♡ <63897347+ktnk-dev@users.noreply.github.com>
2026-01-10 15:06:24 +0300

    лучше билдит

--- build.py
+++ build.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import os
 import sys
+import time
 
-ADB = 'D:\\ADB\\adb.exe'
+BASE = 'flet build'
+ARGS = '-v'
 TARGET = [
     'apk',
     # 'web'
 ]
 
+ADB = 'D:\\ADB\\adb.exe'
+
+_start = time.perf_counter()
 os.system('uv sync')
 for target in TARGET:
-    os.system(f'uv run flet build {target} -vv')
+    os.system(f'uv run {BASE} {target} {ARGS}')
     if target == 'apk' and sys.platform == 'win32':
         os.system(f'{ADB} install build\\apk\\app-release.apk')
+_end = time.perf_counter()
+print(f'\n\nBuilded in {_end-_start}s')
